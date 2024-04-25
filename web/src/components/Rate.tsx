bd60dea4c5ff5c6140f8b86b4aeeb5a34777d23a
ozangurer <ozangurer12@gmail.com>
2024-04-25 16:11:04 +0300

    rate i n detail page

---
+++ web/src/components/Rate.tsx
@@ -0,0 +1,30 @@
+import { IoIosStar } from "react-icons/io";
+import { Type } from "./ui/typography/type";
+
+type RateProps = {
+  avarage: number;
+  count: number;
+};
+
+const Rate = ({ avarage = 1, count = 1 }: RateProps) => {
+  const array = Array.from({ length: Math.floor(avarage) }, () => avarage);
+
+  return (
+    <div className="flex gap-4 items-center">
+      <div className="flex gap-1">
+        {array.map((_item, index) => (
+          <IoIosStar key={index} className="text-accent" size={24} />
+        ))}
+      </div>
+      <div className="flex items-center gap-1">
+        <Type thickness={"semibold"}>{avarage}</Type>
+
+        <Type size={"xs"} variant={"fade-1"}>
+          ({count})
+        </Type>
+      </div>
+    </div>
+  );
+};
+
+export default Rate;
--- web/src/pages/movie-tv-detail/components/DetailInformation.tsx
+++ web/src/pages/movie-tv-detail/components/DetailInformation.tsx
@@ -4,18 +4,27 @@ import { Type } from "@/components/ui/typography/type";
 import { RootState } from "@/services/redux/store";
 import { useSelector } from "react-redux";
 import CastAndCrews from "./CastAndCrews";
+import Rate from "@/components/Rate";
 
 const DetailInformation = () => {
   const { movieDetail } = useSelector((state: RootState) => state.movie);
 
   return (
     <div className="flex flex-col gap-8">
-      <div>
-        <Title size={"2xl"} thickness={"bold"}>
-          {movieDetail?.title}
-        </Title>
-        {movieDetail?.title !== movieDetail?.original_title ? (
-          <Title variant={"fade-1"}>{movieDetail?.original_title}</Title>
+      <div className="flex justify-between items-center">
+        <div>
+          <Title size={"2xl"} thickness={"bold"}>
+            {movieDetail?.title}
+          </Title>
+          {movieDetail?.title !== movieDetail?.original_title ? (
+            <Title variant={"fade-1"}>{movieDetail?.original_title}</Title>
+          ) : null}
+        </div>
+        {movieDetail?.vote_average ? (
+          <Rate
+            avarage={movieDetail?.vote_average}
+            count={movieDetail.vote_count}
+          />
         ) : null}
       </div>
       <div className="flex gap-2">
